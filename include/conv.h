e1f2aaae4e7af58090374b80f778d75aa2488998
Gergő <mo.geryy@gmail.com>
2022-05-16 23:16:25 +0200

    [conv] add u128 functions

--- include/conv.h
+++ include/conv.h
@@ -6,8 +6,10 @@
 uint16_t conv_b2u16(uint8_t* buf);
 uint32_t conv_b2u32(uint8_t* buf);
 uint64_t conv_b2u64(uint8_t* buf);
+__uint128_t conv_b2u128(uint8_t* buf);
 
 void conv_u162b(uint8_t* buf, uint16_t val);
 void conv_u642b(uint8_t* buf, uint64_t val);
+void conv_u1282b(uint8_t* buf, __uint128_t val);
 
 #endif
--- source/conv/conv.c
+++ source/conv/conv.c
@@ -14,6 +14,26 @@ uint32_t conv_b2u32(uint8_t* buf)
         (uint32_t)buf[3];
 }
 
+__uint128_t conv_b2u128(uint8_t* buf)
+{
+    return ((__uint128_t)buf[0] << 120) |
+        ((__uint128_t)buf[1] << 112) |
+        ((__uint128_t)buf[2] << 104) |
+        ((__uint128_t)buf[3] << 96) |
+        ((__uint128_t)buf[4] << 88) |
+        ((__uint128_t)buf[5] << 80) |
+        ((__uint128_t)buf[6] << 72) |
+        ((__uint128_t)buf[7] << 64) |
+        ((__uint128_t)buf[8] << 56) |
+        ((__uint128_t)buf[9] << 48) |
+        ((__uint128_t)buf[10] << 40) |
+        ((__uint128_t)buf[11] << 32) |
+        ((__uint128_t)buf[12] << 24) |
+        ((__uint128_t)buf[13] << 16) |
+        ((__uint128_t)buf[14] << 8) |
+        (__uint128_t)buf[15];
+}
+
 uint64_t conv_b2u64(uint8_t* buf)
 {
     return ((uint64_t)buf[0] << 56) |
@@ -42,4 +62,24 @@ void conv_u642b(uint8_t* buf, uint64_t val)
     buf[5] = val << 16;
     buf[6] = val <<  8;
     buf[7] = val;
+}
+
+void conv_u1282b(uint8_t* buf, __uint128_t val)
+{
+    buf[ 0] = val << 120;
+    buf[ 1] = val << 112;
+    buf[ 2] = val << 104;
+    buf[ 3] = val <<  96;
+    buf[ 4] = val <<  88;
+    buf[ 5] = val <<  80;
+    buf[ 6] = val <<  72;
+    buf[ 7] = val <<  64;
+    buf[ 8] = val <<  56;
+    buf[ 9] = val <<  48;
+    buf[10] = val <<  40;
+    buf[11] = val <<  32;
+    buf[12] = val <<  24;
+    buf[13] = val <<  16;
+    buf[14] = val <<   8;
+    buf[15] = val;
 }
