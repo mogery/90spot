de129d4395dd6b9e53691e6fb3969372d27acb64
Gergő <mo.geryy@gmail.com>
2022-05-17 22:53:52 +0200

    [audiofetch] add AudioFetch api

---
+++ source/spotify/audiofetch.c
@@ -0,0 +1,183 @@
+#include "audiofetch.h"
+#include "log.h"
+
+#include <string.h>
+
+AUDIOFETCH_AUDIO_IV
+
+audiofetch_ctx* audiofetch_init(fetch_ctx* fetch, audiokey_ctx* audiokey)
+{
+    audiofetch_ctx* res = malloc(sizeof(audiofetch_ctx));
+    res->fetch = fetch;
+    res->audiokey = audiokey;
+    res->session = fetch->session;
+    res->requests = NULL;
+
+    return res;
+}
+
+int audiofetch_fetch_data_handler(fetch_ctx* fetch, fetch_pending_request* freq, uint8_t* buf, size_t len, void* _req)
+{
+    audiofetch_request* req = _req;
+
+    uint8_t* dec = malloc(len);
+    aes128CtrCrypt(&req->enc, dec, buf, len);
+
+    if (req->fbuf == NULL)
+    {
+        len -= 0xA7;
+        buf += 0xA7;
+    }
+
+    if (req->fbuf == NULL)
+        req->fbuf = malloc(len);
+    else
+        req->fbuf = realloc(req->fbuf, req->flen + len);
+    memcpy(req->fbuf + req->flen, dec + 0xA7, len);
+    req->flen += len;
+
+    if (req->vorbis == NULL)
+    {
+        int datablock_memory_consumed, error;
+
+        stb_vorbis* try = stb_vorbis_open_pushdata(
+            req->fbuf,
+            req->flen,
+            &datablock_memory_consumed,
+            &error,
+            NULL
+        );
+
+        if (try == NULL)
+        {
+            if (error == VORBIS_need_more_data)
+            {
+                log_info("[AUDIOFETCH] Needs more data...\n");
+            }
+            else
+            {
+                log_error("[AUDIOFETCH] VORBIS error %i\n", error);
+                return -1;
+            }
+        }
+        else
+            req->vorbis = try;
+    }
+    
+    if (req->vorbis != NULL)
+    {
+        int bytes_used, samples_output;
+        while (true)
+        {
+            int channels;
+            float** output;
+
+            bytes_used = stb_vorbis_decode_frame_pushdata(
+                req->vorbis,
+                req->fbuf, req->flen,
+                &channels,
+                &output,
+                &samples_output
+            );
+
+            log_info("[AUDIOFETCH] Bytes: %i Samples: %i Channels: %i\n", bytes_used, samples_output, channels);
+
+            if (bytes_used == 0)
+                break;
+            
+            req->flen -= bytes_used;
+            memcpy(req->fbuf, req->fbuf + bytes_used, req->flen);
+            req->fbuf = realloc(req->fbuf, req->flen);
+
+            if (req->frame_handler != NULL && req->frame_handler(req->ctx, req, &output, channels, req->handler_state) < 0)
+                return -1;
+            
+            // TODO: output memory leak
+        }
+    }
+
+    //free(dec);
+
+    return 0;
+}
+
+int audiofetch_fetch_end_handler(fetch_ctx* fetch, fetch_pending_request* freq, void* _req)
+{
+    audiofetch_request* req = _req;
+
+    log_info("[AUDIOFETCH] Fetch ended!\n");
+    
+    return 0;
+}
+
+int audiofetch_audiokey_response_handler(audiokey_ctx* audiokey, uint8_t* key, size_t len, void* _req)
+{
+    audiofetch_request* req = _req;
+
+    req->key = key;
+    aes128CtrContextCreate(&req->enc, key, audio_iv);
+
+    char track_str[SPOTIFY_ID_B62_LENGTH + 1];
+    spotify_id_to_b62(track_str, req->track);
+    log_info("[AUDIOFETCH] Received key for track %s! Doing initial fetch...", track_str);
+
+    req->fetch = fetch_stream(
+        req->ctx->fetch,
+        req->file,
+        0, 1024 * 16 / FETCH_BLOCK_SIZE, 
+        audiofetch_fetch_data_handler,
+        audiofetch_fetch_end_handler,
+        req
+    );
+
+    return 0;
+}
+
+audiofetch_request* audiofetch_create(
+    audiofetch_ctx* ctx,
+    spotify_id track,
+    spotify_file_id file,
+    audiofetch_frame_handler frame_handler,
+    void* state
+)
+{
+    audiofetch_request* req = malloc(sizeof(audiofetch_request));
+    req->ctx = ctx;
+
+    req->track = track;
+    req->file = file;
+
+    req->key = NULL;
+
+    req->fetch = NULL;
+    req->vorbis = NULL;
+
+    req->fbuf = NULL;
+    req->flen = 0;
+
+    req->frame_handler = frame_handler;
+    req->handler_state = state;
+
+    char track_str[SPOTIFY_ID_B62_LENGTH + 1];
+    spotify_id_to_b62(track_str, track);
+    log_info("[AUDIOFETCH] Initializing request for track %s...", track_str);
+
+    audiokey_request(ctx->audiokey, track, file, audiofetch_audiokey_response_handler, req);
+
+    req->next = ctx->requests;
+    ctx->requests = req;
+    return req;
+}
+
+void audiofetch_destroy(audiofetch_ctx* ctx)
+{
+    audiofetch_request* rptr = ctx->requests;
+    while (rptr != NULL)
+    {
+        audiofetch_request* rnext = rptr->next;
+        free(rptr);
+        rptr = rnext;
+    }
+
+    free(ctx);
+}
---
+++ source/spotify/audiofetch.h
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <switch/crypto/aes_ctr.h>
+#include "session.h"
+#include "fetch.h"
+#include "audiokey.h"
+#include "idtool.h"
+#include "stb_vorbis.h"
+
+#ifndef _audiofetch_H
+#define _audiofetch_H
+
+#define AUDIOFETCH_AUDIO_IV static uint8_t audio_iv[0x10] = {0x72, 0xe0, 0x67, 0xfb, 0xdd, 0xcb, 0xcf, 0x77, 0xeb, 0xe8, 0xbc, 0x64, 0x3f, 0x63, 0x0d, 0x93};
+
+struct audiofetch_ctx {
+    fetch_ctx* fetch;
+    audiokey_ctx* audiokey;
+    session_ctx* session;
+
+    struct audiofetch_request* requests;
+};
+
+// Return <0 for an error.
+typedef int (*audiofetch_frame_handler)(
+    struct audiofetch_ctx* ctx, // The Fetch context the response originates from
+    struct audiofetch_request* req, // The Fetch request the response originates from
+    float*** output,
+    int channels,
+    void* state // Optional state arg
+);
+
+typedef int (*audiofetch_end_handler)(
+    struct audiofetch_ctx* ctx, // The Fetch context the response originates from
+    struct audiofetch_request* req, // The Fetch request the response originates from
+    void* state // Optional state arg
+);
+
+struct audiofetch_request {
+    struct audiofetch_ctx* ctx;
+
+    spotify_id track;
+    spotify_file_id file;
+
+    uint8_t* key;
+    Aes128CtrContext enc;
+
+    fetch_pending_request* fetch;
+    stb_vorbis* vorbis;
+
+    uint8_t* fbuf;
+    size_t flen;
+
+    audiofetch_frame_handler frame_handler;
+    void* handler_state;
+
+    struct audiofetch_request* next;
+};
+
+typedef struct audiofetch_ctx audiofetch_ctx;
+typedef struct audiofetch_request audiofetch_request;
+
+audiofetch_ctx* audiofetch_init(fetch_ctx* fetch, audiokey_ctx* audiokey);
+audiofetch_request* audiofetch_create(audiofetch_ctx* ctx, spotify_id track, spotify_file_id file, audiofetch_frame_handler frame_handler, void* state);
+void audiofetch_destroy(audiofetch_ctx* ctx);
+
+#endif
